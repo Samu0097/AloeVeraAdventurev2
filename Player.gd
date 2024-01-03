7cdb06bc56deec5648271debed08d3519f662c22
McBong69 <officialelrondmcbong@gmail.com>
2024-01-03 15:18:17 +0100

    SkinControl im PlayerScript
    Auf "Tab" kann nun der Skin gewechselt werden

--- Player.gd
+++ Player.gd
@@ -10,6 +10,12 @@ var JumpAvailability : bool
 @onready var animated_sprite = $AnimatedSprite2D
 @onready var JumpTimer : Timer = $JumpTimer
 
+@onready var level_sprite_sets: Array = [] #Alle SpriteSets
+var current_level : int = 0  # Der aktuelle Level-Index
+ 
+
+func _ready():
+	load_sprite_sets()
 
 func _physics_process(delta):
 	# Add the gravity.
@@ -47,6 +53,28 @@ func _physics_process(delta):
 
 	move_and_slide()
 	
+	# Hier könntest du deine Level-Logik einfügen
+	# Zum Beispiel könntest du den Level-Index basierend auf bestimmten Bedingungen ändern
+
+	# Annahme: Ändere den Level-Index, wenn eine Taste gedrückt wird (z.B., für Testzwecke)
+	if Input.is_action_just_pressed("ui_focus_next"):
+		set_sprite_set((current_level + 1) % level_sprite_sets.size())
+	
 
 func _on_jump_timer_timeout():
 	JumpAvailability = false
+
+func load_sprite_sets():
+	# SpriteSets für unterschiedliche Level hinzu
+	level_sprite_sets.append(load("res://Assets/AloeChar/autumn/autumn.tres"))
+	level_sprite_sets.append(load("res://Assets/AloeChar/normal/player.tres"))
+	level_sprite_sets.append(load("res://Assets/AloeChar/nude/nude.tres"))
+	level_sprite_sets.append(load("res://Assets/AloeChar/pink/pink.tres"))
+	level_sprite_sets.append(load("res://Assets/AloeChar/purple/purple.tres"))
+	level_sprite_sets.append(load("res://Assets/AloeChar/terminator-dead/dead.tres"))
+	level_sprite_sets.append(load("res://Assets/AloeChar/xmas/xmas.tres"))
+	
+func set_sprite_set(level_index: int):
+	if level_index < level_sprite_sets.size():
+		animated_sprite.frames = level_sprite_sets[level_index]
+		current_level = level_index
