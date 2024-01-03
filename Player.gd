142d1c73163196a6d444abba84c750e0adb6b9b0
Hohro <niklas-laurent.groeschel@mni.thm.de>
2024-01-03 15:04:34 +0100

    CojoteTimer

--- Player.gd
+++ Player.gd
@@ -6,19 +6,27 @@ const JUMP_VELOCITY = -250.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var JumpAvailability : bool
 @onready var animated_sprite = $AnimatedSprite2D
+@onready var JumpTimer : Timer = $JumpTimer
 
 
 func _physics_process(delta):
 	# Add the gravity.
-	if not is_on_floor():
+	if not is_on_floor() and JumpTimer.is_stopped():
 		velocity.y += gravity * delta
 		animated_sprite.play("fall")   #fall animation
+		
+	if is_on_floor():
+		JumpAvailability = true
+	elif JumpAvailability and JumpTimer.is_stopped():
+		JumpTimer.start()
 
 	# Handle jump.
-	if Input.is_action_just_pressed("Jump") and is_on_floor():
+	if Input.is_action_just_pressed("Jump") and JumpAvailability:
 		velocity.y = JUMP_VELOCITY
 		animated_sprite.play("jump")   #jump animation
+		
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -38,3 +46,7 @@ func _physics_process(delta):
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+	
+
+func _on_jump_timer_timeout():
+	JumpAvailability = false
